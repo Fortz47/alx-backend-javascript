69ad9705056d56f1c983afc9a56de3af940454b0
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-05 22:36:30 +0100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    let value = array[idx];
-    array[idx] = appendString + value;
+  for (let idx of array) {
+    let value = appendString + value;
   }
 
   return array;
