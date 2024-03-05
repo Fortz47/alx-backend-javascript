f6ee8ba347f27b32d2691f1b131ccef8ea22b33f
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-05 23:22:34 +0100

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 export default function createEmployeesObject(departmentName, employees) {
-  return {[`${departmentName}`]: employees}
+  return {[`${departmentName}`]: employees};
 }
