f48843954c197d14e29a65ccc385dd2f7a777762
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-05 21:04:26 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 export default function taskBlock(trueOrFalse) {
-  const var task = false;
-  const var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
