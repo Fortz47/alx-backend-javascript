ec50d59372c9a3c4677de2ba7f8f54fb8582a1eb
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-06 18:35:33 +0000

    solved task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,9 +2,9 @@
 
 export default function handleResponseFromAPI(promise) {
   const obj = { status: 200, body: 'Success' };
-  const err = new Error('');
+  const err = new Error();
   promise
     .then(() => obj)
-    .then(() => console.log('Got a response from the API'))
-    .catch(() => err);
+    .catch(() => err)
+    .finally(() => console.log('Got a response from the API'));
 }
