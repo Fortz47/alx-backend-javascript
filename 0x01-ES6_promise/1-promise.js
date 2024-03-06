18e08d325397cf546bdd9a54949a565ccf196efd
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-06 18:38:41 +0100

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+export default getFullResponseFromAPI(success) {
+  const myPromise = new Promise((resolve, reject) => {
+    if (success)
+      resolve({status: 200, body: 'Success'});
+    else
+      reject(error('The fake API is not working currently'));
+  })
+  return myPromise;
+}
