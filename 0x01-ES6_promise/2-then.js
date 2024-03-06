adf23c00885fbedaf1d82c4cdddf772bfb1d0e5d
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-06 19:39:14 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 export default function handleResponseFromAPI(promise) {
-  const obj = { status: 200, body: 'Success' };
-  const err = new Error();
   promise
-    .then(() => obj)
-    .catch(() => err)
+    .then(() => { status: 200, body: 'Success' })
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
