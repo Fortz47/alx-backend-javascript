6b2d5e3e8d091797c78f5fed7238109ff95db417
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-06 09:15:32 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,10 @@
+#!/usr/bin/node
+
 export default function createReportObject(employeesList) {
-    return {
-        allEmployees: employeesList,
-        getNumberOfDepartments (employeesList) {
-            return Object.keys(employeesList).length;
-        }
-    };
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments (employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
 }
