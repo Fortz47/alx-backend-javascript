3124f44f7552599c42212518ed1afc49a51f76d4
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-06 18:20:28 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 export function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		resolve("foo");
-	});
+  return new Promise((resolve, reject) => {
+    resolve("foo");
+  });
 }
