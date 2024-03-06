b34683afad7cf07e2b06e62f08cab5306ce2a58d
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-06 09:39:58 +0100

    solved 2nd advanced task

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
 export default function iterateThroughObject(reportWithIterator) {
-  
+  const arr = [...(Object.values(report.allEmployees))];
+  let toString = arr[0];
+  for (let 1 = 1; i < arr.length; i += 1) {
+    toString += ` | ${arr[i]}`;
+  }
+
+  return toString;
 }
