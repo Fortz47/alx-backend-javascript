91716a0df955bc9ae9a5665fc338694acd120fd3
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-07 04:52:18 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,15 +3,12 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  const array = [];
-  return Promise.allSettled(promises)
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => {
-      res.forEach((obj) => {
+      res.map((obj) => {
         const value = (obj.status === 'fulfilled') ? res.value : res.reason;
-        array.push({ status: obj.status, value });
+        return { status: obj.status, value };
       });
-      return array;
     });
 }
