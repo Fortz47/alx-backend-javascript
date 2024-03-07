b16beef4cd2aede1c2e03e196a53a7acf8fd2958
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-07 04:50:08 +0100

    Updated 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ import signUpUser from './4-user-promise';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   const array = [];
-  Promise.allSettled(promises)
+  return Promise.allSettled(promises)
     .then((res) => {
       res.forEach((obj) => {
         const value = (obj.status === 'fulfilled') ? res.value : res.reason;
