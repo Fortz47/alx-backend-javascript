45ee7dc1a8547949f4ae0163d57a2b428e5df45c
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-07 04:54:43 +0000

    solveed advanced task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+import { uploadPhoto, createUser } from './utils'
+
+export default async function asyncUploadUser() {
+  result = {};
+  try {
+    result.photo = await uploadPhoto();
+    result.user = await createUser();
+    return result;
+  } catch (err) {
+    return { photo: null, user: null };
+  }
+}
