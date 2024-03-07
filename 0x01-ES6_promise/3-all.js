7656b3b320efa0817ec514c7bda24522cc199d0c
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-07 02:27:12 +0000

    solved task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto()
+    .then((obj) => {
+      process.stdout.write(`${obj.body} `);
+      return createUser();
+    })
+    .then((obj) => {
+      process.stdout.write(` ${obj.firstName}`);
+      process.stdout.write(` ${obj.lastName}\n`);
+    })
+    .catch(() => console.error('Signup system offline'));
+}
