652c85df3f3b66d77f620e602af3aba886db42cf
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-07 05:55:47 +0100

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,11 +3,8 @@
 import { uploadPhoto, createUser } from './utils'
 
 export default async function asyncUploadUser() {
-  result = {};
   try {
-    result.photo = await uploadPhoto();
-    result.user = await createUser();
-    return result;
+    return { photo: await uploadPhoto(), user: await createUser() };
   } catch (err) {
     return { photo: null, user: null };
   }
