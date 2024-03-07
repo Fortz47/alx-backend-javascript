4f95eb4e23ae0bb769111c04760f08c61cd5b3cb
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-07 16:25:29 +0100

    Updated 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,12 +2,15 @@
 
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms {
+export default function initializeRooms() {
   const arr = [];
   for (const val of [19, 20, 34]) {
     arr.push(new ClassRoom(val));
   }
-  return arr.forEach((v) => {
-    `ClassRoom  { _maxStudentsSize: ${v._maxStudentsSize}}`;
-  })
+  const result = [];
+  for (const obj of arr) {
+    const [key, val] = Object.entries(obj)[0];
+    result.push(`ClassRoom { ${key}: ${val} }`);
+  }
+  return result;
 }
