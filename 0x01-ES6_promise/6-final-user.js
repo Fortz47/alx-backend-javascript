ab186f9959fa15e9b20ae12c5deac340ee5f3bfd
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-07 03:26:01 +0000

    solved task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.settled(promises);
+}
