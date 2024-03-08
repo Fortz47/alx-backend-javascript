889428c585f733af81bf788ef0f4e1403439337e
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-08 03:59:44 +0000

    solved task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqrt, floors) {
+    super(sqrt);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    this._floors = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
