4e6178e85facc29bbe1b51fb0838199a4b1e3814
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-08 15:03:44 +0000

    task 101 solved

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   const result = [];
-  Object.values(report.allEmployees).forEach((obj) => {
+  reportWithIterator.forEach((obj) => {
     obj.forEach((v) => result.push(v));
   });
   return result.join(' | ');
