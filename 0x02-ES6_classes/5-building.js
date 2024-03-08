1c6978c99da65214b4103df4decbf405d924f7d1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-08 03:44:33 +0000

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = val;
+  }
+}
