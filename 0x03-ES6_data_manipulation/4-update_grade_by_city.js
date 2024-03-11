3d9c9b0ab9b54be0dc6557fa0a27c8d1dd2885e7
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-11 19:40:10 +0000

    updated task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,10 @@
 export default function updateStudentGradeByCity(listOfStudents, city, newGrades) {
   return listOfStudents.filter((stu) => stu.location === city).map((stu) => {
     newGrades.forEach((v) => {
-      if (v.studentId === stu.id) stu.grade = v.grade;
+      if (v.studentId === stu.id) Object.assign(stu, { grade: v.grade });
     });
-    stu.grade = stu?.grade ?? 'N/A';
+    /* stu.grade = stu?.grade ?? 'N/A'; */
+    if (!stu.grade) Object.assign(stu, { grade: 'N/A' });
     return stu;
   });
 }
