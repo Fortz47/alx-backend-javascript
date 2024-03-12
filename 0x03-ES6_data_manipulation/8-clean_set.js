5e95cdc79ad0eeb35541402de475e5128e8e99a7
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-12 09:22:34 +0000

    updated task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  const re = new RegExp(`^${startString}(.*)`);
+  const str = `^(${startString})(.+)`;
+  const re = new RegExp(str);
   set.forEach((val) => {
-    if (val.startsWith(startString) && startString) {
-      result.push(re.exec(val)[1]);
+    /* if (val.startsWith(startString) && startString) */
+    if (re.exec(val) && re.exec(val)[1] === startString && startString) {
+      result.push(re.exec(val)[2]);
     }
   });
   return result.join('-');
