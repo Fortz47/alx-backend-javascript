2432625950fbd0513f3b79024c2ba0db974d2491
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-02 10:45:29 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const express = require('express');
+
+const app = express();
+
+const port = 7865;
+const host = 'localhost';
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Welcome to the payment system');
+});
+
+app.listen(port, host, () => {
+  console.log(`API available on ${host}:${port}`);
+});
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('Status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Body', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
