e0a595c0bbe921fcdd00409427f8a8ad3f162d8b
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-02 07:54:55 +0100

    task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', function() {
+	it('Utils.calculateNumber should be called with ("SUM", 100, 20)', function() {
+		sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(100, 20);
+		expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+	});
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+function calculateNumber(type, a, b) {
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return Math.round(a) + Math.round(b);
+}
+
+const Utils = {'calculateNumber': calculateNumber};
+module.exports = Utils;
