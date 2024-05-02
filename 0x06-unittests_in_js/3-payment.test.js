fb76f6e5e779ee2b0784ddd5647e4efb04cf7398
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-02 08:02:57 +0100

    task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
 const sinon = require('sinon');
 const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-	it('Utils.calculateNumber should be called with ("SUM", 100, 20)', function() {
-		sinon.spy(Utils, 'calculateNumber');
-		sendPaymentRequestToApi(100, 20);
-		expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-	});
+describe('sendPaymentRequestToApi', () => {
+  it('utils.calculateNumber should be called with ("SUM", 100, 20)', () => {
+    sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+  });
 });
