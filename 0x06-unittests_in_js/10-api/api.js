8954d31eb5a439650889ae48548b6cf595b12969
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-02 13:07:28 +0100

    task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,44 @@
 #!/usr/bin/node
+
+const express = require('express');
+
+const app = express();
+
+// Middleware to Parse the body of the request
+app.use(express.json());
+
+const port = 7865;
+const host = 'localhost';
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  }));
+});
+
+app.post('/login', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Welcome ${req.body.userName}`);
+});
+
+app.listen(port, host, () => {
+  console.log(`API available on ${host}:${port}`);
+});
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,77 @@
 #!/usr/bin/node
+
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('Status code should be 200', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Body should be "Welcome to the payment system"', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('Status code should be 200', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('Body should be "Payment methods for cart 12"', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+  it ('Status code should be 404 when id is not number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
+describe('Available Payments', () => {
+  it('Status code should be 200', (done) => {
+    request('http://localhost:7865/available_payments', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('Body should be {"payment_methods":{"credit_cards":true,"paypal":false}}', (done) => {
+    request('http://localhost:7865/available_payments', (error, response, body) => {
+      expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      done();
+    });
+  });
+});
+
+describe('Login page', () => {
+  const options = {
+    'url': 'http://localhost:7865/login',
+    'method': 'POST',
+    'headers': {'Content-Type': 'application/json'},
+    'body': JSON.stringify({'userName': 'Betty'})
+  };
+  it('Status code should be 200', (done) => {
+    request.post(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('Body should be "Welcome Betty"', (done) => {
+    request.post(options, (error, response, body) => {
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
+  });
+});
