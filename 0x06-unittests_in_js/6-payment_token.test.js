47b237b9b7f5b6a81594f56412cb30cccdf1c48b
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-02 09:48:21 +0100

    task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('returns a resolved promise with a token', (done) => {
+    const result = getPaymentTokenFromAPI(true);
+    expect(result).to.be.an.instanceOf(Promise);
+    result.then((value) => {
+      expect(value).to.eql({ data: 'Successful response from the API' });
+      done();
+    }).catch((error) => done(error));
+  });
+});
