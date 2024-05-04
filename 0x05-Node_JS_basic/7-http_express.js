5f1e011da4a5278b375210984ddb93fb008d82aa
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-04 11:06:50 +0100

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+
+const { createServer } = require('http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    let msg = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((data) => {
+        msg += data;
+        res.end(msg);
+      })
+      .catch((error) => {
+        msg += error.message;
+        res.end(msg);
+      });
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
